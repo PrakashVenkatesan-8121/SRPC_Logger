8550ec6bcd7ba99d57f9e69e71b9c556e78509d6
PK <prakash.vk@zohocorp.com>
2024-07-06 00:37:35 +0530

    SRP Logger engine

---
+++ native/SRPC_LoggerEngine/LoggerEngine/EngineInit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LoggerEngine
+{
+ 
+    class EngineInit
+    {
+
+        static void InitListener()
+        {
+            PipeLineUtil.RecievePipeLineMessage("Zoho_Assist");
+        }
+        static void Main(string[] args)
+        {
+            Thread ListenerThread = new Thread(InitListener);
+            Thread JobProcessor = new Thread(ProcessListenerJob);
+            ListenerThread.Start();
+            JobProcessor.Start();
+            ListenerThread.Join();
+            JobProcessor.Join();
+
+            string name = Console.ReadLine();
+        }
+
+        static void ProcessListenerJob()
+        {
+            while(true)
+            {
+                while(PipeLineUtil.Jobs.Count > 0)
+                {
+                    PipeData job = PipeLineUtil.Jobs.Dequeue();
+                    FileOperator.WriteLog(job.LoggerPath,job.Log);
+                }
+            }
+        }
+    }
+}
